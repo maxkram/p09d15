1c67a7a8b0a0276e7d19dee94da28e991479e1dd
maxkram <max.kramer@live.com>
2025-09-23 11:12:52 +0500

    quest 6

--- main_executable_module/main_executable_module.c
+++ main_executable_module/main_executable_module.c
@@ -20,24 +20,13 @@ typedef void (*sort_func)(double *, int);
 int main() {
   double *data;
   int n;
-
   printf("LOAD DATA...\n");
-  if (scanf("%d", &n) != 1 || n <= 0) {
-    printf("n/a");
-    return 1;
-  }
-
+  if (scanf("%d", &n) != 1 || n <= 0) return printf("n/a"), 1;
   data = (double *)malloc(n * sizeof(double));
-  if (data == NULL) {
-    printf("n/a");
-    return 1;
-  }
-
+  if (data == NULL) return printf("n/a"), 1;
   input(data, n);
-
   printf("RAW DATA:\n\t");
   output(data, n);
-
 #ifdef DYNAMIC
   // Динамическая загрузка библиотеки
   void *handle = dlopen("./data_process.so", RTLD_LAZY);
@@ -46,12 +35,10 @@ int main() {
     free(data);
     return 1;
   }
-
   // Загрузка функций из библиотеки
   normalization_func normalization =
       (normalization_func)dlsym(handle, "normalization");
   sort_func sort = (sort_func)dlsym(handle, "sort");
-
   char *error = dlerror();
   if (error) {
     fprintf(stderr, "Error loading functions: %s\n", error);
@@ -60,35 +47,23 @@ int main() {
     return 1;
   }
 #endif
-
   printf("\nNORMALIZED DATA:\n\t");
   double *normalized_data = (double *)malloc(n * sizeof(double));
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
     normalized_data[i] = data[i];
-  }
-
-  if (normalization(normalized_data, n)) {
+  if (normalization(normalized_data, n))
     output(normalized_data, n);
-  } else {
-    printf("ERROR");
-  }
-
+  else printf("ERROR");
   printf("\nSORTED NORMALIZED DATA:\n\t");
   sort(normalized_data, n);
   output(normalized_data, n);
-
   printf("\nFINAL DECISION:\n\t");
-  if (make_decision(data, n)) {
-    printf("YES");
-  } else {
-    printf("NO");
-  }
+  if (make_decision(data, n)) printf("YES");
+  else printf("NO");
   printf("\n");
-
 #ifdef DYNAMIC
   dlclose(handle);
 #endif
-
   free(data);
   free(normalized_data);
   return 0;
